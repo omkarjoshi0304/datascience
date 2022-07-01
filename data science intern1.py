c4236ed3d86495efbe8ada1e982a75f231b1d149
omkarjoshi0304 <103182931+omkarjoshi0304@users.noreply.github.com>
2022-07-01 20:25:47 +0530

    Add files via upload

---
+++ data science intern1.py
@@ -0,0 +1,76 @@
+'''x=13
+m=6
+y="89"
+z="2"
+print(type(y))
+print(type(x))
+print(type(m))
+print (x+m)
+print (float(y+z))
+print (str(x+m))
+
+
+a = 7
+print(type(a))
+
+
+b = 3.0
+print(type(b))
+
+
+c = a + b
+print(c)
+print(type(c))
+
+
+d = a * b
+print(d)
+print(type(d))
+
+r=2.3
+print (str(r))
+print (type(r))'''
+
+#list
+list1=[1,3,4,'hi',5.8,[15,'joshi',13,1.7,'car']]
+'''print(list1[-3:-1])
+print(list[1:6])
+l2=list1[5][3]
+print(l2)
+if 'hi' in list1:
+   print('yes')
+list1[2]=69
+print(list1)
+list1[1:3]="hello",4.5
+print(list1)
+list1.insert(3,'fast')
+print(list1)
+list1.append('dog')
+print(list1)
+list1.insert(2,4.5)
+print(list1)
+list3=['google','Microsoft']
+list1.extend(list3)
+print(list1)
+list1.remove(3)
+print(list1)
+list1.pop(2)
+print(list1)
+del list1[1:4]
+print(list1)
+list1.clear()
+print(list1)
+list1.sort()
+print(list1)'''
+
+
+#tupple
+
+
+#dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
